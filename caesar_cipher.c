c373f79d1b9fdcbb7d22a0e8ad44adb69c9a86d2
Emily Washer <z5061368@student.unsw.edu.au>
2020-03-09 20:26:21 +1100

    Updated version of caesar cipher and files of brief research

---
+++ caesar_cipher.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BUF_SIZE 1024
+
+void encode_message(char* message, int key);
+
+int main (int argc, char* argv[]){
+    int key = 0;
+    char *message = malloc(sizeof(char) * BUF_SIZE);
+   
+    printf("Enter a number betrween 1-26: ");
+    scanf("%d", &key);
+    if (1 > key || key > 26){
+        fprintf(stderr, "Invalid key entered. Please enter a number between 1 and 26\n");
+        exit(1);
+    }
+    
+    getchar();
+    printf("Enter a message to encode: ");
+    
+    while (fgets(message, BUF_SIZE, stdin) != NULL){
+        encode_message(message, key);
+        if (message[strlen(message) - 1] == '\n') break;
+    }
+       
+    printf("Your encoded message is: %s", message);
+    free(message);
+}
+
+void encode_message(char* message, int key){
+    for (int i = 0; i < (int)strlen(message); i++){
+        if (message[i] >= 'a' && message[i] <= 'z'){
+            if ((message[i] + key) > 'z'){
+                message[i] = (message[i] + key - 'z' + 'a' - 1);
+            } else {
+                message[i] = (message[i] + key); 
+            }
+        } else if (message[i] >= 'A' && message[i] <= 'Z'){
+            if ((message[i] + key) > 'Z'){
+                message[i] = (message[i] + key - 'Z' + 'A' - 1);
+            } else {
+                message[i] = (message[i] + key); 
+            }
+        }
+    }
+}
