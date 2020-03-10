9c722fa2b7c174e55145f2d135029c9fe7781af3
EmilyWasher <emilywasher@gmail.com>
2020-03-10 13:59:56 +1100

    Removed key variable

--- atbash_cipher.c
+++ atbash_cipher.c
@@ -4,24 +4,23 @@
 
 #define BUF_SIZE 1024
 
-void encode_message(char* message, int key);
+void encode_message(char* message);
 
 int main (int argc, char* argv[]){
     printf("Welocome to the Atbash cipher\n");
-    int key = 0;
     char *message = malloc(sizeof(char) * BUF_SIZE);
     printf("Enter a message to encode: ");
     
     while (fgets(message, BUF_SIZE, stdin) != NULL){
-        encode_message(message, key);
+        encode_message(message);
         if (message[strlen(message) - 1] == '\n') break;
     }
        
-    printf("Your encoded message is: %s", message);
+    printf("Your new message is: %s", message);
     free(message);
 }
 
-void encode_message(char* message, int key){
+void encode_message(char* message){
     for (int i = 0; i < (int)strlen(message); i++){
         if ('a' <= message[i] && message[i] <= 'z'){
             message[i] = 'z' + 'a' - message[i];
