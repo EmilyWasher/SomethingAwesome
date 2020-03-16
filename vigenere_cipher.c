02c54068dce750a95bedb1c6de0421ca86c1ec05
Emily Washer <z5061368@student.unsw.edu.au>
2020-03-17 10:37:10 +1100

    the program now encodes messages

--- vigenere_cipher.c
+++ vigenere_cipher.c
@@ -4,11 +4,10 @@
 
 #define ALPHA 26
 #define CODE 10
-#define LC_to_UC 'a' - 'A'
 #define BUF 1024
-//balh
-void encode_message(char* message);
-void convert_to_lc (char code_word[]);
+
+void encode_message(char* message, int code[], int code_len, char matrix[ALPHA][ALPHA]);
+void convert_to_num (char code_word[], int code[]);
 void create_matrix(char matrix[ALPHA][ALPHA]);
 
 int main (int argc, char* argv[]){
@@ -17,27 +16,20 @@ int main (int argc, char* argv[]){
 
 	create_matrix(matrix);
 
-
 	printf("Welcome to the Vigenere cipher\nEnter a code word (<10): ");
 	char code_word[CODE]; 
 	scanf("%s", &code_word);
 	getchar();
-	int code_len = strlen(code_word);
-	printf("%s\n", code_word);
+	int code_length = strlen(code_word);
+	int code[code_length];
 
-	convert_to_lc(code_word);
-
-	/*for (int i = 0; i < strlen(code_word); i++){
-		//code_word[i] = code_word[i] - 'a';
-		//printf ("%d ", code_word[i]);
-		printf("%d", code_word[i] );
-	}*/
+	convert_to_num(code_word, code);
 
 	printf("Enter your message to encode: ");
 	
 	while (fgets(message, BUF, stdin) != NULL){
-		encode_message(message);
-		//if (message[strlen(message) - 1] == '\n') break;
+		encode_message(message, code, code_length, matrix);
+		if (message[strlen(message) - 1] == '\n') break;
 	}
 
 	printf("Your endcoded mesage is: %s", message);
@@ -47,19 +39,25 @@ int main (int argc, char* argv[]){
 	return 0;
 }
 
-void encode_message(char* message){
-	/*if ('a' <= message[i] && message[i] < ='z'){
-
-	} else if ('A' <= message[i] && message[i] <= 'Z'){
-		printf("wassup");
-	}*/
-	printf("%s\n", message);
+void encode_message(char* message, int code[], int code_len, char matrix[ALPHA][ALPHA]){
+	int is_letter = 0;
+	for (int i = 0; i < strlen(message); i++){
+	    if ('a' <= message[i] && message[i] <='z'){
+            message[i] = matrix[code[is_letter % code_len]][message[i] - 'a'];
+            is_letter++;
+	    } else if ('A' <= message[i] && message[i] <= 'Z'){
+		    message[i] = matrix[code[is_letter % code_len]][message[i] - 'A'] - 'a' + 'A';
+            is_letter++;
+	    }
+	}
 }
 
-void convert_to_lc (char code_word[]){	
+void convert_to_num (char code_word[], int code[]){	
 	for (int i = 0; i <strlen(code_word); i++){
 		if ('A' <= code_word[i] && code_word[i] <= 'Z'){
-			code_word[i] = code_word[i] + 'a' - 'A';
+			code[i] = code_word[i] - 'A';
+		} else if ('a' <=code_word[i] && code_word[i] <= 'z'){
+		    code[i] = code_word[i] - 'a';
 		}
 	}
 }
@@ -72,5 +70,6 @@ void create_matrix(char matrix[ALPHA][ALPHA]){
 			} else {
 				matrix[i][j] = 'a' + i + j;
 			}
-		}	}
+		}	
+	}
 }
