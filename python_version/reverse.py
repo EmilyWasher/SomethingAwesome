999b0a215da87a419261a0e4e896d5c06b7fb833
EmilyWasher <emilywasher@gmail.com>
2020-03-29 12:56:21 +1100

    can now reverse whole sentences or by word

---
+++ python_version/reverse.py
@@ -0,0 +1,22 @@
+def get_input():
+    print("Welcome to the reverse cipher")
+    message = input("Enter the message you would like to encode: ")
+    return message
+
+
+def rev_by_word(message):
+    return ' '.join(message.split()[::-1])
+
+
+def rev_by_letter(message):
+    l = list(message)
+    l.reverse()
+    return ''.join(l)
+
+
+message = get_input()
+word = rev_by_word(message)
+letter = rev_by_letter(message)
+
+print("BY word is: ", word)
+print("By letter is: ", letter)
